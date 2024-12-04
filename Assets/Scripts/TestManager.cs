25c014333f8c7aebfcda8025c3a3f97e81fdeb40
RobertMa123 <rmatzkin@clarku.edu>
2024-12-04 15:48:02 -0500

    Added button to scroll thru tutorial

--- Assets/Scripts/TestManager.cs
+++ Assets/Scripts/TestManager.cs
@@ -18,10 +18,13 @@ public class TestManager : MonoBehaviour
     private bool scoreIsShowing = false;
     private bool tutorialIsShowing = false;
 
+    private InputData input;
 
     // Start is called before the first frame update
     void Start()
     {
+        input = GetComponent<InputData>();
+
         if (resultsUI != null) resultsUI.testScoreViewer.SetActive(false);
         if (tutorialUI != null) tutorialUI.tutorialViewer.SetActive(false);
 
@@ -38,12 +41,12 @@ public class TestManager : MonoBehaviour
         }
 
         if (scoreIsShowing) {
-            if (Keyboard.current.fKey.wasPressedThisFrame) {
+            if (Keyboard.current.fKey.wasPressedThisFrame || (input._leftController.TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out bool pressed) || (input._rightController.TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out bool pressedR)) && (pressed || pressedR))) {
                 hideScores();
             }
         }
         if (tutorialIsShowing) {
-            if (Keyboard.current.fKey.wasPressedThisFrame) {
+            if (Keyboard.current.fKey.wasPressedThisFrame || (input._leftController.TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out bool pressed) || (input._rightController.TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out bool pressedR)) && (pressed || pressedR))) {
                 tutorialNextPage();
             }
         }
